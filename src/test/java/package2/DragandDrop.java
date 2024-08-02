201eeeb690224e3b4aa82d7b4efda5feb8cc4094
Kiran Patil <kiran.patil@yexle.com>
2024-08-02 17:34:09 +0530

    training practise project

---
+++ src/test/java/package2/DragandDrop.java
@@ -0,0 +1,52 @@
+package package2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class DragandDrop {
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://testautomationpractice.blogspot.com/");
+        WebElement dragElement = driver.findElement(By.id("draggable"));
+        WebElement dropElement = driver.findElement(By.id("droppable"));
+        Actions act = new Actions(driver);
+        act.dragAndDrop(dragElement,dropElement).build().perform();
+        WebElement linkopen = driver.findElement(By.xpath("//a[normalize-space()='orange HRM']"));
+       String parentwindowID = driver.getWindowHandle();
+        act.keyDown(Keys.CONTROL).click(linkopen).keyUp(Keys.CONTROL).build().perform();
+        Set<String> ids = driver.getWindowHandles();
+//        List<String> windowIds = new ArrayList(ids);
+//        String childWindowId = windowIds.get(1);
+//        System.out.println("Child window Id is "+childWindowId );
+//        driver.switchTo().window(childWindowId);
+//        String childwindowTitle = driver.getTitle();
+//        System.out.println(childwindowTitle);
+
+        for(String handle:ids){
+            if(!handle.equals(parentwindowID)){
+                driver.switchTo().window(handle);
+                break;
+            }
+        }
+        String childWindowTitle = driver.getTitle();
+        System.out.println(childWindowTitle);
+
+
+
+
+
+
+    }
+}
